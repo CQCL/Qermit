049429d352fae74ed24f015e1056628e5d31a461
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2021-05-27 12:41:30 +0100

    Add documentation to GitHub pages (#1)
    * Add .gitignore file
    
    * Update doc and manual build scripts
    
    Now build in qermit/docs/build/html
    
    * Fix math notation in manual_mitex.rst
    
    ``G -> ``G``
    
    * Add Docs
    
    * Add manual build

--- manual/build_manual.sh
+++ manual/build_manual.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sed "s/REQUIREMENTS/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' ../requirements.txt | tr -d '\n')/" index-rst-template > index.rst
-rm -rf ./_build
-sphinx-build -b html ./ ./_build
+rm -rf ../docs/build/html/manual
+sphinx-build -b html ./ ../docs/build/html/manual
 
 rm index.rst
