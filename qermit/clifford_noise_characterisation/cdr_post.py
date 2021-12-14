77b537dba016afa957f57112207dfc2139ae3e30
Daniel Mills <danielmills@Daniels-MacBook-Pro.local>
2021-12-14 18:29:31 +0000

    Remove unnecessary prints from cdr_post.py

--- qermit/clifford_noise_characterisation/cdr_post.py
+++ qermit/clifford_noise_characterisation/cdr_post.py
@@ -109,9 +109,6 @@ def cdr_quality_check_task_gen(distance_tolerance: float, calibration_fraction: 
 
         for calibration, original in zip(state_circuit_exp, noisy_expectation):
 
-            print("original", original, sep='\n')
-            print("calibration", *calibration, sep='\n')
-
             # The noisy expectation value of the original circuit.
             original_coefficient = original.to_list()[0]["coefficient"][0]
 
@@ -130,7 +127,6 @@ def cdr_quality_check_task_gen(distance_tolerance: float, calibration_fraction: 
 
             # Raise a warning if the calibration circuits regularly have noisy
             # expectation value far from the original circuit.
-            print("is_far_count", is_far_count, "len(calibration)", len(calibration), "calibration_fraction", calibration_fraction)
             if is_far_count > len(calibration) * calibration_fraction:
                 warnings.warn(
                     "Training data regularly differs significantly from original circuit. Fit may be poor."
@@ -191,8 +187,6 @@ def cdr_calibration_task_gen(
                 # go through strings in operator
                 for key in noisy_qpo._dict:
                     # make sure keys are present (don't initialise at start incase indexing missing)
-                    # if abs(exact_qpo[key]) > tolerance:
-                    # print(f"{noisy_qpo} is all good")
                     if key not in noisy_char_dict:
                         noisy_char_dict[key] = list()
                     if key not in exact_char_dict:
@@ -204,8 +198,6 @@ def cdr_calibration_task_gen(
 
                     noisy_char_dict[key].append(float(noisy_qpo._dict[key]))
                     exact_char_dict[key].append(float(exact_qpo._dict[key]))
-                    # else:
-                    #     print(f"{noisy_qpo} is too small")
             if backend.backend_info is None:
                 raise ValueError("Backend has no backend_info attribute.")
 
