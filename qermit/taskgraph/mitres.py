efe52dd2130046ae2e6812572ef5424ac4f3175a
Daniel Mills <danielmills@Daniels-MacBook-Pro.local>
2022-02-25 21:05:05 +0000

    Use process_circuit

--- qermit/taskgraph/mitres.py
+++ qermit/taskgraph/mitres.py
@@ -81,11 +81,13 @@ def backend_handle_task_gen(backend: Backend) -> MitTask:
         """
 
         if len(circuit_wires) != 0:
-            circs, shots = map(list, zip(*circuit_wires))
+            circs, n_shots_list = map(list, zip(*circuit_wires))
 
-            results = backend.process_circuits(
-                circs, n_shots=cast(Sequence[int], shots)
-            )
+            # results = backend.process_circuits(
+            #     circs, n_shots=cast(Sequence[int], n_shots_list)
+            # )
+
+            results = [backend.process_circuit(circ, n_shots) for circ, n_shots in zip(circs, n_shots_list)]
 
             return (results,)
         else:
