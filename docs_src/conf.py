667456fce51b6c38eb0aa8e3acc7c2dcb33f2df9
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2023-03-10 14:55:14 +0000

    bump version to 0.3.0 (#73)

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -22,9 +22,9 @@ copyright = "2021, Cambridge Quantum Computing Ltd"
 author = "Cambridge Quantum Computing Ltd"
 
 # The short X.Y version
-version = "0.2.0"
+version = "0.3.0"
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.3.0"
 
 
 # -- General configuration ---------------------------------------------------
