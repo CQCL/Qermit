d5c7557283396d4cb64b7675e03d3ab69585b5a2
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-05-30 15:08:53 +0100

    update manual config

--- manual/conf.py
+++ manual/conf.py
@@ -4,7 +4,6 @@
 # See https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 project = "qermit-manual"
-copyright = "2020-2021 Cambridge Quantum Computing Ltd"
 author = "Cambridge Quantum Computing Ltd"
 
 extensions = [
@@ -14,4 +13,17 @@ extensions = [
     "jupyter_sphinx",
 ]
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+
+html_theme_options = {
+    "repository_url": "https://github.com/CQCL/qermit",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "logo": {
+        "image_light": "_static/Quantinuum_logo_black.png",
+        "image_dark": "_static/Quantinuum_logo_white.png",
+    },
+}
+
+html_static_path = ["../_static"]
+html_css_files = ["custom.css"]
