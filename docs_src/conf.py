00f5750d65d5c0cced2573148f9c8556d76dc40b
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2023-08-08 12:28:21 +0100

    add source code links to API docs

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -41,6 +41,7 @@ extensions = [
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
     "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
