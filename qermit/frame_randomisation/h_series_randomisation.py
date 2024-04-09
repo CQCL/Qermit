b86ce612bab4da287963db8642920f52476d1717
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-04-09 10:58:48 +0100

    Move to using classical expressions

--- qermit/frame_randomisation/h_series_randomisation.py
+++ qermit/frame_randomisation/h_series_randomisation.py
@@ -21,35 +21,34 @@ def gen_randomised_circuit(circuit):
     for c_register in circuit.c_registers:
         randomised_circuit.add_c_register(c_register)
 
-    # seed_size = 16
-    # seed_c_reg = BitRegister(name='seed_c', size=seed_size)
-    # randomised_circuit.add_c_register(seed_c_reg)
+    seed_size = 16
+    seed_c_reg = BitRegister(name='seed_c', size=seed_size)
+    randomised_circuit.add_c_register(seed_c_reg)
 
-    # if circuit.n_qubits < seed_size:
-    #     seed_q_reg = QubitRegister(
-    #         name='seed_q',
-    #         size=seed_size-sum(q_register.size for q_register in randomised_circuit.q_registers)
-    #     )
-    #     randomised_circuit.add_q_register(seed_q_reg)
+    if circuit.n_qubits < seed_size:
+        seed_q_reg = QubitRegister(
+            name='seed_q',
+            size=seed_size-sum(q_register.size for q_register in randomised_circuit.q_registers)
+        )
+        randomised_circuit.add_q_register(seed_q_reg)
 
-    # all_qubits = list(itertools.chain.from_iterable([q_register.to_list() for q_register in randomised_circuit.q_registers]))
-    # for qubit, cbit in zip(all_qubits[:seed_size], seed_c_reg.to_list()):
-    #     randomised_circuit.H(qubit)
-    #     randomised_circuit.Measure(qubit, cbit)
-    #     randomised_circuit.Reset(qubit)
-    # randomised_circuit.add_wasm_to_reg("seed_randomisation", wfh, [seed_c_reg], [])
+    all_qubits = list(itertools.chain.from_iterable([q_register.to_list() for q_register in randomised_circuit.q_registers]))
+    for qubit, cbit in zip(all_qubits[:seed_size], seed_c_reg.to_list()):
+        randomised_circuit.H(qubit)
+        randomised_circuit.Measure(qubit, cbit)
+        randomised_circuit.Reset(qubit)
+    randomised_circuit.add_wasm_to_reg("seed_randomisation", wfh, [seed_c_reg], [])
 
-    randomisation_reg = randomised_circuit.add_c_register("randomisation", 8)
+    randomisation_reg = randomised_circuit.add_c_register("randomisation", 4)
 
-    rand_vals = [116, 154]
+    # rand_vals = [10, 7]
+    # command_count = 0
 
-    for command_count, command in enumerate(circuit):
-
-        # randomisation_reg = randomised_circuit.add_c_register(f"randomisation_{command_count}", 8)
+    for command in circuit:
 
         if command.op.type == OpType.ZZMax:
-            # randomised_circuit.add_wasm_to_reg("write_randomisation", wfh, [], [randomisation_reg])
-            randomised_circuit.add_c_setreg(rand_vals[command_count], randomisation_reg)
+            randomised_circuit.add_wasm_to_reg("write_randomisation", wfh, [], [randomisation_reg])
+            # randomised_circuit.add_c_setreg(rand_vals[command_count], randomisation_reg)
             randomised_circuit.Z(command.qubits[0], condition=randomisation_reg[0])
             randomised_circuit.Z(command.qubits[1], condition=randomisation_reg[1])
             randomised_circuit.X(command.qubits[0], condition=randomisation_reg[2])
@@ -58,10 +57,21 @@ def gen_randomised_circuit(circuit):
         randomised_circuit.add_gate(command.op, command.args)
 
         if command.op.type == OpType.ZZMax:
-            randomised_circuit.Z(command.qubits[0], condition=randomisation_reg[4])
-            randomised_circuit.Z(command.qubits[1], condition=randomisation_reg[5])
-            randomised_circuit.X(command.qubits[0], condition=randomisation_reg[6])
-            randomised_circuit.X(command.qubits[1], condition=randomisation_reg[7])
+            randomised_circuit.Z(
+                command.qubits[0],
+                condition=randomisation_reg[0] ^ randomisation_reg[2] ^ randomisation_reg[3]
+            )
+            randomised_circuit.Z(
+                command.qubits[1],
+                condition=randomisation_reg[1] ^ randomisation_reg[2] ^ randomisation_reg[3]
+            )
+            randomised_circuit.X(command.qubits[0], condition=randomisation_reg[2])
+            randomised_circuit.X(command.qubits[1], condition=randomisation_reg[3])
+            
+            # randomised_circuit.Z(command.qubits[0], condition=randomisation_reg[4])
+            # randomised_circuit.Z(command.qubits[1], condition=randomisation_reg[5])
+            # randomised_circuit.X(command.qubits[0], condition=randomisation_reg[6])
+            # randomised_circuit.X(command.qubits[1], condition=randomisation_reg[7])
 
             # randomised_circuit.Z(command.qubits[0], condition=randomisation_reg[0])
             # randomised_circuit.Z(command.qubits[0], condition=randomisation_reg[2])
@@ -73,8 +83,10 @@ def gen_randomised_circuit(circuit):
             # randomised_circuit.Z(command.qubits[1], condition=randomisation_reg[3])
             # randomised_circuit.X(command.qubits[1], condition=randomisation_reg[3])
 
-            # randomised_circuit.Phase(0.5, condition=randomisation_reg[2])
-            # randomised_circuit.Phase(0.5, condition=randomisation_reg[3])
-            # randomised_circuit.Phase(-1, condition_bits=[randomisation_reg[2], randomisation_reg[3]])
+            randomised_circuit.Phase(0.5, condition=randomisation_reg[2])
+            randomised_circuit.Phase(0.5, condition=randomisation_reg[3])
+            randomised_circuit.Phase(-1, condition=randomisation_reg[2] & randomisation_reg[3])
+
+            # command_count += 1
     
     return randomised_circuit, wfh
--- qermit_rus/src/lib.rs
+++ qermit_rus/src/lib.rs
@@ -9,42 +9,43 @@ fn init(){
     // It can also be used to set up a global state.
 }
 
-fn get_bit(bit_string: u8, index: u32) -> u8 {
-    let base: u8 = 2;
-    (bit_string & base.pow(index)) >> index
-}
+// fn get_bit(bit_string: u8, index: u32) -> u8 {
+//     let base: u8 = 2;
+//     (bit_string & base.pow(index)) >> index
+// }
 
-fn get_correction(initial_pauli: u8) -> u8 {
+// fn get_correction(initial_pauli: u8) -> u8 {
     
-    // calculate x_1
-    let mut correct_pauli: u8 = get_bit(initial_pauli, 3);
-    correct_pauli = correct_pauli << 1;
+//     // calculate x_1
+//     let mut correct_pauli: u8 = get_bit(initial_pauli, 3);
+//     correct_pauli = correct_pauli << 1;
 
-    // calculate x_0
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
-    correct_pauli = correct_pauli << 1;
+//     // calculate x_0
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
+//     correct_pauli = correct_pauli << 1;
 
-    // calculate z_1
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 1);
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 3);
-    correct_pauli = correct_pauli << 1;
+//     // calculate z_1
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 1);
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 3);
+//     correct_pauli = correct_pauli << 1;
 
-    // calculate z_0
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 0);
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
-    correct_pauli = correct_pauli ^ get_bit(initial_pauli, 3);
+//     // calculate z_0
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 0);
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 2);
+//     correct_pauli = correct_pauli ^ get_bit(initial_pauli, 3);
 
-    correct_pauli
-}
+//     correct_pauli
+// }
 
 #[no_mangle]
 pub extern "C" fn write_randomisation() -> u8 {
     
-    let initial_pauli: u8 = fastrand::u8(..) >> 4;
-    let correct_pauli: u8 = get_correction(initial_pauli);
+    // let initial_pauli: u8 = fastrand::u8(..);
+    // let correct_pauli: u8 = get_correction(initial_pauli);
 
-    initial_pauli | correct_pauli << 4
+    // initial_pauli | correct_pauli << 4
+    fastrand::u8(..)
 }
 
 #[no_mangle]
@@ -52,16 +53,16 @@ pub extern "C" fn seed_randomisation(seed: u16) {
     fastrand::seed(seed as u64);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn correct_correction() {
-        assert_eq!(get_correction(14), 14);
-        assert_eq!(get_correction(9), 10);
-        assert_eq!(get_correction(4), 7);
-        assert_eq!(get_correction(2), 2);
-        assert_eq!(get_correction(6), 5);
-    }
-}
+//     #[test]
+//     fn correct_correction() {
+//         assert_eq!(get_correction(14), 14);
+//         assert_eq!(get_correction(9), 10);
+//         assert_eq!(get_correction(4), 7);
+//         assert_eq!(get_correction(2), 2);
+//         assert_eq!(get_correction(6), 5);
+//     }
+// }
