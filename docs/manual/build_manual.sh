aac21218b2a93d7a302649d0ea9ba8ee324217ad
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-10-02 22:39:31 +0100

    add manual index to docs index

--- docs/manual/build_manual.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sed "s/REQUIREMENTS/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' ../requirements.txt | tr -d '\n')/" index-rst-template > index.rst
-rm -rf ../docs/manual
-sphinx-build -b html ./ ../docs/manual
-rm index.rst
