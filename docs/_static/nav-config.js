59bd417b45bf7629da660fdd6f2010de592a8c92
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-10-02 22:34:28 +0100

    add navbar config

---
+++ docs/_static/nav-config.js
@@ -0,0 +1,21 @@
+
+const navConfig = {
+    "navTextLinks": [
+        {
+            "title": "API docs",
+            "href": "index.html",
+        },
+        {
+            "title": "Manual",
+            "href": "manual/index.html",
+        },
+    ],
+    "navProductName": "Qermit",
+    "navIconLinks": [
+        {
+            "title": "Qermit Github",
+            "href": "https://github.com/CQCL/qermit",
+            "iconImageURL": "_static/assets/github.svg",
+        },
+    ],
+}
