c1d56e72dd1822cccef51a35be2c8d917346f43f
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-11-21 17:29:50 +0000

    Add NAPC and tests

---
+++ qermit/coherent_pauli_checks/__init__.py
@@ -0,0 +1,12 @@
+from .clifford_detect import (  # noqa:F401
+    QermitDAGCircuit,
+    cpc_rebase_pass,
+)
+from .pauli_sampler import (  # noqa:F401
+    PauliSampler,
+    DeterministicZPauliSampler,
+    DeterministicXPauliSampler,
+    RandomPauliSampler,
+    OptimalPauliSampler,
+    CircuitPauliChecker,
+)
