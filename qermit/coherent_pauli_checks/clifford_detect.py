d72d156e6d01474d9fb1aba364952e40e59d3246
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-11-21 17:56:36 +0000

    Fix some formatting

--- qermit/coherent_pauli_checks/clifford_detect.py
+++ qermit/coherent_pauli_checks/clifford_detect.py
@@ -1,27 +1,23 @@
-from pytket import Circuit, OpType, Qubit
-import networkx as nx  # type: ignore
-from pytket.circuit import Command, CircBox  # type: ignore
-from pytket.passes.auto_rebase import auto_rebase_pass
 import math
-from typing import Optional, List, Union, cast
+from typing import List, Optional, Union, cast
 
+import networkx as nx  # type: ignore
+from pytket import Circuit, OpType, Qubit
+from pytket.circuit import CircBox, Command
+from pytket.passes.auto_rebase import auto_rebase_pass
 
 clifford_ops = [OpType.CZ, OpType.H, OpType.Z, OpType.S, OpType.X]
 non_clifford_ops = [OpType.Rz]
 
-cpc_rebase_pass = auto_rebase_pass(
-    gateset=set(clifford_ops + non_clifford_ops)
-)
+cpc_rebase_pass = auto_rebase_pass(gateset=set(clifford_ops + non_clifford_ops))
 
 
 class DAGCommand:
-
     def __init__(self, command: Command) -> None:
         self.command = command
 
     @property
     def clifford(self) -> bool:
-
         if self.command.op.is_clifford_type():
             return True
 
@@ -35,16 +31,12 @@ class DAGCommand:
 
 
 class QermitDAGCircuit(nx.DiGraph):
-
     def __init__(self, circuit: Circuit, cutoff: Optional[int] = None) -> None:
-
         # TODO: There are other things to be saved, like the phase.
 
         super().__init__()
 
-        self.node_command = [
-            DAGCommand(command) for command in circuit.get_commands()
-        ]
+        self.node_command = [DAGCommand(command) for command in circuit.get_commands()]
         self.qubits = circuit.qubits
         self.bits = circuit.bits
         self.cutoff = cutoff
@@ -65,7 +57,6 @@ class QermitDAGCircuit(nx.DiGraph):
                 current_node[qubit] = node
 
     def get_clifford_subcircuits(self) -> List[int]:
-
         # a list indicating the clifford subcircuit to which a command belongs.
         node_sub_circuit: List[Union[int, None]] = [None] * self.number_of_nodes()
         next_sub_circuit_id = 0
@@ -73,7 +64,6 @@ class QermitDAGCircuit(nx.DiGraph):
         # Iterate through all commands and check if their neighbours should
         # be added to the same clifford subcircuit.
         for node, command in enumerate(self.node_command):
-
             # If the command is not in a clifford sub circuit, start a
             # new one and add it to that new one,
             if node_sub_circuit[node] is None:
@@ -88,7 +78,6 @@ class QermitDAGCircuit(nx.DiGraph):
             # neighbour to the same clifford subcircuit if no non-clifford
             # gates prevent this from happening.
             for neighbour_id in self.neighbors(node):
-
                 # Do not add the neighbour if it is not a clifford gate.
                 if not self.node_command[neighbour_id].clifford:
                     continue
@@ -104,7 +93,8 @@ class QermitDAGCircuit(nx.DiGraph):
                 # list all of the commands in the circuit which belong to
                 # the same sub circuit as the one being considered
                 same_sub_circuit_node_list = [
-                    i for i, sub_circuit in enumerate(node_sub_circuit)
+                    i
+                    for i, sub_circuit in enumerate(node_sub_circuit)
                     if sub_circuit == node_sub_circuit[node]
                 ]
 
@@ -116,7 +106,6 @@ class QermitDAGCircuit(nx.DiGraph):
                 # sub circuits sequentially.
                 same_clifford_circuit = True
                 for same_sub_circuit_node in same_sub_circuit_node_list:
-
                     # I'm allowing to pass cutoff, but that should
                     # not be allowed. In particular paths of arbitrary
                     # lengths should be checked in practice.
@@ -125,7 +114,6 @@ class QermitDAGCircuit(nx.DiGraph):
                     for path in nx.all_simple_paths(
                         self, same_sub_circuit_node, neighbour_id, cutoff=self.cutoff
                     ):
-
                         if not all(
                             node_sub_circuit[path_node] == node_sub_circuit[node]
                             for path_node in path[:-1]
@@ -139,7 +127,9 @@ class QermitDAGCircuit(nx.DiGraph):
                 if same_clifford_circuit:
                     node_sub_circuit[neighbour_id] = node_sub_circuit[node]
 
-        if any(sub_circuit is None for sub_circuit in node_sub_circuit):  # pragma: no cover
+        if any(
+            sub_circuit is None for sub_circuit in node_sub_circuit
+        ):  # pragma: no cover
             raise Exception("Some nodes have been left unassigned.")
 
         return cast(List[int], node_sub_circuit)
@@ -147,7 +137,6 @@ class QermitDAGCircuit(nx.DiGraph):
     # TODO: I'm not sure if this should return a circuit, or changes this
     # QermitDagCircuit in place
     def to_clifford_subcircuit_boxes(self) -> Circuit:
-
         # TODO: It could be worth insisting that the given circuit does not
         # include any boxes called 'Clifford Subcircuit'. i.e. that the
         # circuit is 'clean'.
@@ -166,13 +155,13 @@ class QermitDAGCircuit(nx.DiGraph):
             clifford_box_circuit.add_bit(bit)
 
         while not all(implemented_commands):
-
             # Search for a subcircuit that it is safe to implement, and
             # pick the first one found to be implemented.
             not_implemented = [
                 node_sub_circuit
-                for node_sub_circuit, implemented
-                in zip(node_sub_circuit_list, implemented_commands)
+                for node_sub_circuit, implemented in zip(
+                    node_sub_circuit_list, implemented_commands
+                )
                 if not implemented
             ]
             sub_circuit_to_implement = None
@@ -186,28 +175,27 @@ class QermitDAGCircuit(nx.DiGraph):
 
             # List the nodes in the chosen sub circuit
             node_to_implement_list = [
-                node for node in self.nodes()
+                node
+                for node in self.nodes()
                 if node_sub_circuit_list[node] == sub_circuit_to_implement
             ]
             assert len(node_to_implement_list) > 0
 
             # If the circuit is clifford add it as a circbox
             if self.node_command[node_to_implement_list[0]].clifford:
-
                 # Empty circuit to contain clifford subcircuit
                 clifford_subcircuit = Circuit(
-                    n_qubits=len(
-                        sub_circuit_qubits[sub_circuit_to_implement]
-                    ),
-                    name='Clifford Subcircuit'
+                    n_qubits=len(sub_circuit_qubits[sub_circuit_to_implement]),
+                    name="Clifford Subcircuit",
                 )
 
                 # Map from qubits in original circuit to qubits in new
                 # clifford circuit.
                 qubit_to_index = {
                     qubit: i
-                    for i, qubit
-                    in enumerate(sub_circuit_qubits[sub_circuit_to_implement])
+                    for i, qubit in enumerate(
+                        sub_circuit_qubits[sub_circuit_to_implement]
+                    )
                 }
 
                 # Add all gates to new circuit
@@ -217,14 +205,14 @@ class QermitDAGCircuit(nx.DiGraph):
                         [
                             qubit_to_index[qubit]
                             for qubit in self.node_command[node].command.args
-                        ]
+                        ],
                     )
                     implemented_commands[node] = True
 
                 clifford_circ_box = CircBox(clifford_subcircuit)
                 clifford_box_circuit.add_circbox(
                     clifford_circ_box,
-                    list(sub_circuit_qubits[sub_circuit_to_implement])
+                    list(sub_circuit_qubits[sub_circuit_to_implement]),
                 )
 
             # Otherwise, add the gates straight to the circuit
@@ -232,7 +220,7 @@ class QermitDAGCircuit(nx.DiGraph):
                 for node in node_to_implement_list:
                     clifford_box_circuit.add_gate(
                         self.node_command[node].command.op,
-                        self.node_command[node].command.args
+                        self.node_command[node].command.args,
                     )
                     implemented_commands[node] = True
 
@@ -268,10 +256,8 @@ class QermitDAGCircuit(nx.DiGraph):
         node_sub_circuit_list: List[int],
         implemented_commands: List[bool],
     ) -> bool:
-
         can_implement = True
         for node in self.nodes:
-
             if not node_sub_circuit_list[node] == sub_circuit:
                 continue
 
