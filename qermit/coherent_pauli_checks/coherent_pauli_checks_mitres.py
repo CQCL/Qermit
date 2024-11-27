57ad1bcf0a7fd1974f165fb18a90f8548a05ad7d
Dan Mills <52407433+daniel-mills-cqc@users.noreply.github.com>
2024-11-27 15:57:57 +0000

    Document pauli check mitres

--- qermit/coherent_pauli_checks/coherent_pauli_checks_mitres.py
+++ qermit/coherent_pauli_checks/coherent_pauli_checks_mitres.py
@@ -12,7 +12,20 @@ from .pauli_sampler import PauliSampler
 
 
 def gen_find_cliffords_task() -> MitTask:
-    def task(obj, circ_shots_list: List[CircuitShots]) -> Tuple[List[CircuitShots]]:
+    """Generator for MitTask for finding Clifford subcircuits within
+    given circuits. Inputs and outputs are CircuitShots. Output circuits
+    have Clifford sub circuits wrapped in CircBox.
+
+    :return: Task for finding Clifford subcircuits.
+    """
+
+    def task(_, circ_shots_list: List[CircuitShots]) -> Tuple[List[CircuitShots]]:
+        """Function wrapping Clifford subcircuits in CircBox.
+
+        :param circ_shots_list: List of circuit shots.
+        :return: Identical circuits with Clifford circuit wrapped
+            in CircBox.
+        """
         cliff_circ_shots_list = []
 
         for circ_shots in circ_shots_list:
@@ -28,17 +41,28 @@ def gen_find_cliffords_task() -> MitTask:
         return (cliff_circ_shots_list,)
 
     return MitTask(
-        _label="FindCliffords",
+        _label="FindCliffordSubcircuits",
         _n_in_wires=1,
         _n_out_wires=1,
         _method=task,
     )
 
 
-def gen_check_circuit_task(pauli_sampler) -> MitTask:
+def gen_check_circuit_task(pauli_sampler: PauliSampler) -> MitTask:
+    """Generator for MitTask applying Pauli checks to circuits.
+
+    :param pauli_sampler: PauliSampler to use to sample checks.
+    :return: Task for adding Pauli checks.
+    """
+
     def task(
-        obj, circ_shots_list: List[CircuitShots]
+        _, circ_shots_list: List[CircuitShots]
     ) -> Tuple[List[CircuitShots], List[PostselectMgr]]:
+        """Task for adding Pauli checks to given circuits.
+
+        :param circ_shots_list: Circuits to add Pauli checks to.
+        :return: Circuits with pauli checks added.
+        """
         checked_circ_shots_list = []
         postselect_mgr_list = []
 
@@ -77,6 +101,14 @@ def gen_coherent_pauli_check_mitres(
     backend: Backend,
     pauli_sampler: PauliSampler,
 ) -> MitRes:
+    """Generator for MitRes performing mitigation through
+    Coherent Pauli Checks.
+
+    :param backend: Backend to perform perform experiment with.
+    :param pauli_sampler: Sampler to use to apply Pauli checks.
+    :return: MitRes performing mitigation through
+    Coherent Pauli Checks.
+    """
     _mitres = MitRes(backend, _label="PostselectionMitRes")
     _taskgraph = TaskGraph().from_TaskGraph(_mitres)
 
