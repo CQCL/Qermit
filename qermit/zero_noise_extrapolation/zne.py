5564214cbfcc680d97abcdf0e924522894aa267e
Melf <cqc@melf.de>
2025-10-30 13:23:33 +0000

    update qiksit version, use different compilation path

--- qermit/zero_noise_extrapolation/zne.py
+++ qermit/zero_noise_extrapolation/zne.py
@@ -970,7 +970,7 @@ def gen_initial_compilation_task(
 
             cu = CompilationUnit(compiled_circ)
             backend.default_compilation_pass(
-                optimisation_level=optimisation_level
+                optimisation_level=optimisation_level, allow_symbolic=True,
             ).apply(cu)
             node_map = cu.final_map
             node_map_list.append(node_map)
