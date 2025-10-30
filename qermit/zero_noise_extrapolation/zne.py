229da3217614bfb1428f800f8e790f5e692f6eb3
Melf <cqc@melf.de>
2025-10-30 16:57:04 +0000

    fix mypy

--- qermit/zero_noise_extrapolation/zne.py
+++ qermit/zero_noise_extrapolation/zne.py
@@ -969,7 +969,7 @@ def gen_initial_compilation_task(
             compiled_circ = obs_exp.AnsatzCircuit.Circuit.copy()
 
             cu = CompilationUnit(compiled_circ)
-            backend.default_compilation_pass(
+            backend.default_compilation_pass(  # type: ignore
                 optimisation_level=optimisation_level, allow_symbolic=True,
             ).apply(cu)
             node_map = cu.final_map
